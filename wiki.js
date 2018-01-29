63e2d13047317047dc216b1197498f78ecaa4107
Christopher Parsons <mail@christopherparsons.me>
2018-01-29 23:33:10 +0000

    Refactoring Adjusted the url into two variables to make the whole $.getJSON function easier to read.

--- wiki.js
+++ wiki.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 
 var input = '';
+var urlPart1 = 'https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch='
+var urlPart2 = '&prop=extracts&exlimit=10&exintro&exsentences=2&format=json&callback=?'
 
 function wiki () {
-    $.getJSON('https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=' + 'input' + '&prop=extracts&exlimit=10&exintro&exsentences=2&format=json&callback=?', function(response){
+    $.getJSON(urlPart1 + 'input' + urlPart2, function(response){
       var output = response.query.pages;
       console.log(output);
     });
