7aff4f05bcc66780b7e77862c5c197fe0b41d640
Christopher Parsons <mail@christopherparsons.me>
2018-02-01 00:37:04 +0000

    Search terms I've updated the html and javascript to allow end users to input their own search term. This is pushed into the API to get a result.

--- wiki.js
+++ wiki.js
@@ -4,10 +4,9 @@ var input = '';
 var urlPart1 = 'https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch='
 var urlPart2 = '&prop=extracts&exlimit=10&exintro&exsentences=2&format=json&callback=?'
 
-function wiki () {
-    $.getJSON(urlPart1 + 'input' + urlPart2, function(response){
+function wiki (input) {
+    $.getJSON(urlPart1 + input + urlPart2, function(response){
       var output = response.query.pages;
-      console.log(output);
     });
     // $.ajax('https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=' + 'input' + '&prop=extracts&exlimit=10&exintro&exsentences=2&format=json&callback=?',
     //   {
@@ -17,9 +16,16 @@ function wiki () {
     //     }
     // });
     // The ajax method above matches what I've used previously in the quote project.
-    // However, the getJSON method works as well due to the lack of an API key.
+    // However, the getJSON method works as well due to the lack of a required API key.
   }
 
-  wiki ();
+  $('#submit').on('click', function() {
+    // Should only run when the search button is clicked.
+    let search = document.getElementById('search').value;
+    // Here I am pulling the search field from the text input field.
+    wiki(search);
+    // Pushing the search variable into the function that's contacting the
+    // Wikipedia API.
+  });
 
 })
