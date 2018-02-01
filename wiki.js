3248a40d13f4864789874b535fdd5726ffb246ca
Christopher Parsons <mail@christopherparsons.me>
2018-02-01 23:15:43 +0000

    Updating html Added some html elements which are being updated from an array in js.

--- wiki.js
+++ wiki.js
@@ -1,14 +1,33 @@
 $(document).ready(function() {
 
-var input = '';
-var urlPart1 = 'https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch='
-var urlPart2 = '&prop=extracts&exlimit=10&exintro&exsentences=2&format=json&callback=?'
+const urlPart1 = 'https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch='
+const urlPart2 = '&prop=extracts&exlimit=10&exintro&exsentences=2&format=json&callback=?'
 
 function wiki (input) {
     $.getJSON(urlPart1 + input + urlPart2, function(response){
-      var output = response.query.pages;
+      let output = response.query.pages;
+      console.log(output);
+      let titles = [];
+      let extract = [];
+      let id = [];
+      // Creating my arrays to hold the various elements of the object that I need.
+      for (let key in output) {
+        if (output.hasOwnProperty(key)) {
+          titles.push(output[key]['title']);
+          extract.push(output[key]['extract']);
+          id.push(output[key]['pageid']);
+        }
+      }
+      // Using the for loop to hold the values I want from the JSON object.
+      $('#title1').text(titles[0]);
+      $('#extract1').text(extract[0]);
+      $('#link1').attr('href',"")
+      // For now, I am just filling in one result from the search to confirm it's working as expected.
+      // Will eventually iterate through my arrays of content to show all results.
+      console.log(titles);
+      console.log(extract);
     });
-    // $.ajax('https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=' + 'input' + '&prop=extracts&exlimit=10&exintro&exsentences=2&format=json&callback=?',
+    // $.ajax('urlPart1' + input + urlPart2',
     //   {
     //     dataType: 'json',
     //     success: function(data) {
@@ -23,6 +42,11 @@ function wiki (input) {
     // Should only run when the search button is clicked.
     let search = document.getElementById('search').value;
     // Here I am pulling the search field from the text input field.
+    console.log(search);
+    if (search == '') {
+      return
+    }
+    //Here I am escaping the function if there is no search entered.
     wiki(search);
     // Pushing the search variable into the function that's contacting the
     // Wikipedia API.
